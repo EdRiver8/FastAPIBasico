dedc8d46ea040bb0298d36123e21d9e0c9d99de2
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-28 20:32:58 -0500

    validations_query

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # pertenece a python como tal
+from operator import le
 from typing import Optional
+from typing_extensions import Required
 
 # antes que fastapi, porque este trabaja con pydantic (gerarquia)
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -24,3 +26,11 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): # '...' significan que el parametro 'Body' es obligatorio
   return person
+
+@app.get("/person/detail") # minimo 1, maximo 50
+def show_person(
+  name: str | None = Query(default=None, alias="name-query", min_length=1, max_length=50),
+  age: str = Query(...)
+): 
+  return {name: age}
+  
