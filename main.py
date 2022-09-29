b2464c3429e5e7ee1a18bed7ce7dc902919b8028
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-28 20:08:59 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+# pertenece a python como tal
+from typing import Optional
+
+# antes que fastapi, porque este trabaja con pydantic (gerarquia)
+from pydantic import BaseModel
+
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+  first_name: str
+  last_name: str
+  age: int
+  hair_color: str | None = Field(default=None, example="Blonde, Dark")
+  is_married: bool | None = None
 
 @app.get("/") # path operation decoration
 def home():
   return {"Hola": "World"}
 
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)): # '...' significan que el parametro 'Body' es obligatorio
+  return person
