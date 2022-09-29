4ca4735e1ee8fb5f3e6105e0946f0a4e1898321a
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-28 20:46:12 -0500

    validations_path

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # pertenece a python como tal
-from operator import le
-from typing import Optional
 from typing_extensions import Required
 
 # antes que fastapi, porque este trabaja con pydantic (gerarquia)
 from pydantic import BaseModel, Field
 
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
@@ -23,14 +21,39 @@ def home():
   return {"Hola": "World"}
 
 # Request and Response Body
+
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): # '...' significan que el parametro 'Body' es obligatorio
   return person
 
+
+#Validaciones: Query Parameters
+
 @app.get("/person/detail") # minimo 1, maximo 50
 def show_person(
-  name: str | None = Query(default=None, alias="name-query", min_length=1, max_length=50),
-  age: str = Query(...)
+  name: str | None = Query(
+    default=None, 
+    alias="name-query", 
+    min_length=1, 
+    max_length=50,
+    description="This is the person name. Its between 1 and 50 characters"
+    ),
+  age: str = Query(
+    ..., 
+    title="Person Age"
+    )
 ): 
   return {name: age}
+
+
+#Validaciones: Path Parameters; en este no se usa max o min, sino gt, le
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+  person_id: int = Path(
+    ..., 
+    gt=0
+    )
+):
+  return {person_id: "It exists!"}
   
